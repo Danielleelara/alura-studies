6d92c36b106c0dfc11dc1e82e48704920ff9b41f
danielleelara <dany.sa1@hotmail.com>
2022-05-19 21:49:04 -0300

    feat: criando lista

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Form from './components/Formulario';
+import Lista from './components/Lista';
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <Lista />
     </div>
   );
 }
