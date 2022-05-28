5398dbf93fda8d419fea2cbbe3c45bd7eed240a4
danielleelara <dany.sa1@hotmail.com>
2022-05-28 19:46:31 -0300

    feat: usando useEffect

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Botao from '../Botao';
 import Relogio from './Relogio';
 import styles from './Cronometro.module.scss';
@@ -12,9 +12,11 @@ interface Props {
 export default function Cronometro({selecionado}: Props) {
   const [tempo, setTempo] = useState<number>();
 
-  if(selecionado?.tempo){
-    setTempo(tempoParaSegundos(selecionado.tempo))
-  }
+  useEffect(()=>{
+    if(selecionado?.tempo){
+    setTempo(tempoParaSegundos(String(selecionado.tempo)));
+    }
+  },[selecionado]);
 
   return (
     <div className={styles.cronometro}>
